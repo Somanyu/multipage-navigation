512d171af25f39a3b48dad95ef88d9a08946f31a
Somanyu <somanyu.03samal@gmail.com>
2021-08-11 11:54:42 +0530

    coming soon

---
+++ src/Product.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+// eslint-disable-next-line
+import { Tabs, Tab, Modal, Row, Button, Col, Nav, Form, Card, Container, Alert } from "react-bootstrap";
+
+
+
+class Product extends Component {
+    render() {
+        return (
+            <div justify id="home">
+                <Container style={{width:"50rem"}}>
+                    <Alert variant="success">
+                        <Alert.Heading>Hey, nice to see you</Alert.Heading>
+                        <p>
+                            Aww yeah, you successfully saw this amazing project of mine. This project
+                            is going to take a bit longer to be developed so that you can see how amazing
+                            this whole product website looks.
+                        </p>
+                        <hr />
+                        <p className="mb-0">
+                            Whenever you need to get the code you can visit my <a href="https://github.com/Somanyu">Github</a>.
+                        </p>
+                    </Alert>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Product;
